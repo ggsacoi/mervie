ad053874e58289e1f38ca4712edc1bb99e9ff52d
ggsacoi <118943639+ggsacoi@users.noreply.github.com>
2024-12-01 07:04:37 +0100

    Mise a jour 6 lancement de jeu 2

--- jeux.js
+++ jeux.js
@@ -1,6 +1,6 @@
 const launch = document.querySelector(".launch");
 const map = document.querySelector(".game");
-launch.addEventListener('click', ()=>{
+launch.addEventListener('click', ()=> {
         map.style.display = "flex";
 });
 map.height = window.innerHeight/2;
@@ -8,13 +8,38 @@ map.width = window.innerWidth/2;
 const context = map.getContext("2d");
 let gravity = 2;
 let velocityX = 1.5;
+let decorvelocityX = 0.8;
 let Objects = [];
+let trucs = [];
 let frameCount = 0;
 let isAnimating = false;
 let animation;
 let scoreCount = 0;
 const obstacleP = map.height/2+70;
 const upstacleP =  map.height/2+70-90;
+let building = {
+    x: map.width,
+    y: map.height/2,
+};
+let sun = {
+    x: map.width/4,
+    y: map.height/4,
+    color: "white",
+    icon: "\uf185",
+    vitesseX: 0.004
+};
+let monblaze = {
+    x: map.width,
+    y: map.height-70,
+    color: 'white',
+    vitesseX: 0.2
+};
+let mervie = {
+    x: map.width/2+map.width,
+    y: map.height-70,
+    color: 'white',
+    vitesseX: 0.2
+};
 function randomXPosition(options) {
     return options[Math.floor(Math.random() * options.length)];
 }
@@ -44,6 +69,24 @@ function upstacleH() {
 }
 function upstacle() {
     Objects.push({ height: 30, width: 30, x: randomXPosition([550, 620]), y: upstacleP, color: 'purple', val: 7000});
+}
+function decor1() {
+    trucs.push({x: building.x+750, y: map.height/2, color: "white", icon: "\uf1ad", velocityX: decorvelocityX});
+}
+function decor2() {
+    trucs.push({x: building.x+800, y: map.height/2, color: "white", icon: '\uf1ad', velocityX: decorvelocityX});
+}
+function decor3() {
+    trucs.push({x: building.x+500, y: map.height/2, color: "white", icon: '\uf1ad', velocityX: decorvelocityX});
+}
+function decor4() {
+    trucs.push({x: building.x, y: map.height/2, color: "white", velocityX: decorvelocityX, icon:"\uf1ad"});
+}
+function decor5() {
+    trucs.push({x: building.x+150, y: map.height/2, color: "white", icon: '\uf1ad', velocityX: decorvelocityX});
+}
+function decor6() {
+    trucs.push({x: building.x+300, y: map.height/2, color: "white", icon: "\uf1ad", velocityX: decorvelocityX});
 }
 function moveobstacle() {
     Objects.forEach((obj) => {
@@ -135,8 +178,67 @@ function initializeObstacles() {
     upstacleH();
     upstacle();
 }
+function thesun() {
+    context.font = "30px FontAwesome";
+    context.fillStyle = sun.color;
+    context.fillText(sun.icon, sun.x, sun.y);
+    sun.x += sun.vitesseX;
+    if (sun.x >= map.width) {
+        sun.x = -30;
+    }
+}
+function drawdecor() {
+    trucs.forEach((truc) => {
+        if(truc.color === "white") {
+            context.font = "30px FontAwesome";
+            context.fillStyle = truc.color;
+            context.fillText(truc.icon, truc.x, truc.y);
+        }
+    })
+}
+function movedecor() {
+    trucs.forEach((truc) =>{
+        if(truc.color === 'white') {
+            truc.x -= truc.velocityX;
+    if (truc.x <= -50) {
+        truc.x = map.width;
+    }
+        }
+    })
+}
+function blaze() {
+    context.font = "20px Aial";
+    context.fillStyle = monblaze.color;
+    context.fillText(`@makilapetoaz0`, monblaze.x, monblaze.y);
+    monblaze.x -= monblaze.vitesseX;
+    if (monblaze.x <= 0-120) {
+        monblaze.x = map.width;
+    }
+}
+function aka() {
+    context.font = "20px Aial";
+    context.fillStyle = mervie.color;
+    context.fillText(`Mervie`, mervie.x, mervie.y);
+    mervie.x -= mervie.vitesseX;
+    if (mervie.x <= 0-100) {
+        mervie.x = map.width;
+    }
+}
+function initializedecor() {
+    decor1();
+    decor2();
+    decor3();
+    decor4();
+    decor5();
+    decor6();
+}
 function animate() {
     space();
+    thesun();
+    blaze();
+    aka();
+    drawdecor();
+    movedecor();
     ground();
     score();
     moveobstacle();
@@ -156,6 +258,10 @@ function reloadAnimation() {
     initializeObstacles();
     mainobject();
     score();
+    thesun();
+    blaze();
+    drawdecor();
+    movedecor();
     animation = requestAnimationFrame(animate);
 }
 document.addEventListener('keydown', (e) => {
@@ -173,5 +279,6 @@ document.addEventListener('keydown', (e) => {
     }
 });
 initializeObstacles()
+initializedecor()
 mainobject()
 animation = requestAnimationFrame(animate);
